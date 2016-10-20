52ac911127c58bc776b949bb56870fad6cec9afb
angbrav <manuelbravogestoso@gmail.com>
2016-10-20 11:31:52 +0100

    add hardcoded special case

--- src/saturn_benchmarks_eventual_da.erl
+++ src/saturn_benchmarks_eventual_da.erl
@@ -254,6 +254,17 @@ run(read, KeyGen, _ValueGen, #state{node=Node,
                                      mydc=MyDc,
                                      total_dcs=NumberDcs}=S0) ->
     {ok, Bucket} = case Correlation of
+        special ->
+            case MyDc of
+                0 ->
+                    {ok, 4};
+                1 ->
+                    {ok, 4};
+                2 ->
+                    {ok, 9};
+                3 ->
+                    {ok, 9}
+            end;
         uniform ->
             pick_local_bucket(uniform, LocalBuckets);
         full ->
@@ -340,6 +351,17 @@ run(update, KeyGen, ValueGen, #state{node=Node,
                                       bucket_full=BucketFull,
                                       local_buckets=LocalBuckets}=S0) ->
     {ok, Bucket} = case Correlation of
+        special ->
+            case MyDc of
+                0 ->
+                    {ok, 4};
+                1 ->
+                    {ok, 4};
+                2 ->
+                    {ok, 9};
+                3 ->
+                    {ok, 9}
+            end;
         uniform ->
             pick_local_bucket(uniform, LocalBuckets);
         full ->
